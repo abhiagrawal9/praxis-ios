b41f322fc5914a679c7c7a758120328553fd640f
abhiagrawal9 <abhi2703agrawal@gmail.com>
2021-05-15 05:54:14 +0530

    initial commit

---
+++ helloworld/Shared/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  Shared
+//
+//  Created by Abhishek Agrawal on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
